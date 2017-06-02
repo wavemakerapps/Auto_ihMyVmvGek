516cdc2a8221343f946c963c41244c2817200c37
root <root@d3a2e0704417>
2017-06-02 01:15:21 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ihMyVmvGek",
+  "homePage" : "Main",
+  "name" : "Auto_ihMyVmvGek",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
